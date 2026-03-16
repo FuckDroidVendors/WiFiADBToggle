b13a0296caa3c4cdf86674c9bd99824fad82b0d1
Hans Müller <schreibemirhalt@gmail.com>
2026-03-16 13:20:27 +0100

    refactor: replace non-full stubs with Java

---
+++ app/src/basic/java/fuck/wifiadbtoggle/droidvendorssuck/MediaButtonTestActivity.java
@@ -0,0 +1,12 @@
+package fuck.wifiadbtoggle.droidvendorssuck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MediaButtonTestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
---
+++ app/src/basic/java/fuck/wifiadbtoggle/droidvendorssuck/ScheduleActivity.java
@@ -0,0 +1,12 @@
+package fuck.wifiadbtoggle.droidvendorssuck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScheduleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
